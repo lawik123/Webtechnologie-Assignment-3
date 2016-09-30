3914507ddd818e967cbc38e84a18ba484614ff34
Lawik Ayoub <428302@student.saxion.nl>
2016-09-30 09:17:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Lawik Ayoub on 26-Sep-16.
+ */
+
+var express = require('express');
+var app = express();
+
+// parse application/json
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+app.get('/api', function (req, res) {
+    res.send('Hello api!');
+});
