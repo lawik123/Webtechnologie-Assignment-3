0c0bd5de94bec952c13555b87945bb0de1df2174
larsmeulenbroek <larsmeulenbroek1997@hotmail.com>
2016-10-30 23:56:55 +0100

    delete top 5

--- public/js/index.js
+++ public/js/index.js
@@ -7,72 +7,10 @@ $(document).ready(function () {
             scrollTop: $("#section1").offset().top
         }, 100);
     });
-    $('#top5nav').click(function () {
-        $('html, body').animate({
-            scrollTop: $("#section2").offset().top
-        }, "slow");
-    });
 
-    if(lscache.get("tokenData")) {
+    if (lscache.get("tokenData")) {
         $(".nav.navbar-nav").append("<li><a class='netflixText' href='community.html'>Community <span class='sr-only'></span></a></li>")
         $(".nav.navbar-nav>li#registerNav").hide();
     }
 
-    var first = 0;
-    var moviecounter = 0;
-
-    $.ajax({
-        url: "http://localhost:3000/api/movies",
-        type: "GET",
-        dataType: "json",
-        success: function (result) {
-            $.each(result, function (key, val) {
-                    var poster;
-                    console.log(val.title);
-                    $.ajax({
-                        type: "GET",
-                        dataType: "json",
-                        url: "http://www.omdbapi.com/?t=" + val.title,
-                        success: function (data) {
-                            poster = data.Poster;
-                        },
-                        async: false,
-                        error: function () {
-                            return "Image not found.";
-                        }
-                    });
-                    console.log(poster);
-                    if (first === 0 && moviecounter < 5) {
-                        moviecounter++;
-                        first = 1;
-                        $("#topFive").append(
-                            "<div class='movie-image'>" +
-                            "<img class='active' width='300' height='450' src='" + poster + "'/>" +
-                            "</div></div></div>")
-                    } else if(moviecounter < 5) {
-                        moviecounter++;
-                        $("#topFive").append(
-                            "<div class='movie-image'>" +
-                            "<img width='300' height='450' src='" + poster + "'/>" +
-                            "</div></div></div>")
-                    }
-                }
-            )
-        }
-
-    });
-    slideSwitch();
-
-    function slideSwitch() {
-        var $active = $('#topFive img.active');
-        var $next = $active.next();
-
-        $next.addClass('active');
-
-        $active.removeClass('active');
-    }
-
-    $(function() {
-        setInterval( "slideSwitch()", 5000 );
-    });
 });
